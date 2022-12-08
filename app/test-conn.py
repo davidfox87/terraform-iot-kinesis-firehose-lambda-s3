5257d559b55fe245ff2f5ab0c7eac50203b07494
David Fox <davidmfox87@gmail.com>
2022-12-07 20:11:19 -0700

    connection script

--- app/test-conn.py
+++ app/test-conn.py
@@ -11,14 +11,53 @@ import os
 
 received_all_event = threading.Event()
 
-target_ep = 'axb6ef1ye7l5s-ats.iot.us-west-2.amazonaws.com'
-thing_name = 'ratchet'
-cert_filepath = './device-crt-and-keys/certificate.pem'
-private_key_filepath = './device-crt-and-keys/privateKey.pem'
+# target_ep = 'axb6ef1ye7l5s-ats.iot.us-west-1.amazonaws.com' # endpoint for the device registered in the cloud
+# thing_name = 'ratchet'
 ca_filepath = './device-crt-and-keys/RSA-AmazonRootCA1.pem'
 
-pub_topic = 'device/{}/data'.format(thing_name)
-sub_topic = 'app/data'
+# pub_topic = 'device/{}/data'.format(thing_name)
+
+
+def parse_arguments(argv):
+        """Parse command line arguments
+        Args:
+        argv (list): list of command line arguments including program name
+        Returns:
+        The parsed arguments as returned by argparse.ArgumentParser
+        """
+        parser = argparse.ArgumentParser(description='Training')
+
+        parser.add_argument('--client_id',
+                        type=str,
+                        help="The ID that uniquely identifies this device in the AWS Region")
+
+        parser.add_argument('--target_endpoint',
+                        type=str,
+                        help="endpoint for the thing device")
+
+        parser.add_argument('--ca_file',
+                        type=str,
+                        help="AmazonRootCA file path")
+
+        parser.add_argument('--key',
+                        type=str,
+                        help="Path to your key in PEM format.")
+
+        parser.add_argument('--cert',
+                        type=str,
+                        help='Path to your client certificate in PEM format.')
+
+        parser.add_argument('--topic',
+                        type=str,
+                        help='topic name to publish messages to')
+        
+        parser.add_argument('--message_string',
+                        type=str,
+                        help='message to publish')
+
+        args, _ = parser.parse_known_args(args=argv[1:])
+
+        return args
 
 
 
@@ -55,29 +94,31 @@ def on_message_received(topic, payload, dup, qos, retain, **kwargs):
 
 def main(argv=None):
         
+    args = parse_arguments(sys.argv if argv is None else argv)
+
     # Spin up resources
     event_loop_group = io.EventLoopGroup(1)
     host_resolver = io.DefaultHostResolver(event_loop_group)
     client_bootstrap = io.ClientBootstrap(event_loop_group, host_resolver)
 
     proxy_options = None
-
+    # establish a connection with AWS IoT Core by using the MQTT protocol
     mqtt_connection = mqtt_connection_builder.mtls_from_path(
-        endpoint=target_ep,
+        endpoint=args.target_endpoint,
         port=8883,
-        cert_filepath=cert_filepath,
-        pri_key_filepath=private_key_filepath,
+        cert_filepath=args.cert,
+        pri_key_filepath=args.key,
         client_bootstrap=client_bootstrap,
-        ca_filepath=ca_filepath,
+        ca_filepath=args.ca_file,
         on_connection_interrupted=on_connection_interrupted,
         on_connection_resumed=on_connection_resumed,
-        client_id=thing_name,
+        client_id=args.client_id,
         clean_session=True,
         keep_alive_secs=30,
         http_proxy_options=proxy_options)
 
     print("Connecting to {} with client ID '{}'...".format(
-        target_ep, thing_name))
+        args.target_endpoint, args.client_id))
 
     #Connect to the gateway
     while True:
@@ -92,31 +133,38 @@ def main(argv=None):
         else:
             print("Connected!")
             break       
+    
 
-    # Subscribe
-    print("Subscribing to topic " + sub_topic)
-    subscribe_future, packet_id = mqtt_connection.subscribe(
-        topic=sub_topic,
-        qos=mqtt.QoS.AT_LEAST_ONCE,
-        callback=on_message_received)
 
-    subscribe_result = subscribe_future.result()
-    print("Subscribed with {}".format(str(subscribe_result['qos'])))
+    pub_topic = args.topic
+    print ('Publishing message on topic {}'.format(pub_topic))
 
-    while True:
-        print ('Publishing message on topic {}'.format(pub_topic))
-        
-        hello_world_message = {
-            'message' : 'Hello from {} in the AWS IoT Workshop'.format(thing_name)
-        }
-        
-        message_json = json.dumps(hello_world_message)
-        mqtt_connection.publish(
-            topic=pub_topic,
-            payload=message_json,
-            qos=mqtt.QoS.AT_LEAST_ONCE)
-        time.sleep(5)
+    message_count = 5
 
+    if args.message_string:
+        if message_count == 0:
+            print ("Sending messages until program killed")
+        else:
+            print ("Sending {} message(s)".format(message_count))
+
+        publish_count = 1
+        while (publish_count <= message_count) or (message_count == 0):
+            message = "{} [{}]".format(args.message_string, publish_count)
+            print("Publishing message to topic '{}': {}".format(args.message_topic, message))
+            message_json = json.dumps(message)
+            mqtt_connection.publish(
+                topic=args.message_topic,
+                payload=message_json,
+                qos=mqtt.QoS.AT_LEAST_ONCE)
+            time.sleep(1)
+            publish_count += 1
+
+
+    # Disconnect
+    print("Disconnecting...")
+    disconnect_future = mqtt_connection.disconnect()
+    disconnect_future.result()
+    print("Disconnected!")
 
 if __name__ == '__main__':
     main()
